f7f6576864212b3672d6072a662e6af61aaa4407
Antony Damico <antonydamico29@gmail.com>
2018-03-26 13:34:05 -0400

    added the remove functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,14 @@
 $("li").click(function(){
 	$(this).toggleClass("done");
 });
+
+$("span").click(function(e){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+
+
+
+
+	e.stopPropagation();
+});
