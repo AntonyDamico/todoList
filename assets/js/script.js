7c49eca62cca2b578c423e0ffd0d4bbe70c5d813
Antony Damico <antonydamico29@gmail.com>
2018-03-26 13:59:38 -0400

    added the add todo functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,23 @@
-//Check off specific todos
-$("li").click(function(){
+//Check off specific todos as done
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("done");
 });
 
-$("span").click(function(e){
+//Delete the todo selected
+$("ul").on("click", "span", function(e){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
-
-
-
-
 	e.stopPropagation();
 });
+
+//Add a new todo pressing enter
+$("input[type='text']").keypress(function(e){
+	if(e.which === 13){
+		var todo = $(this).val();
+		$("ul").append("<li><span>x</span> "+ todo + "</li>");
+		$(this).val("");
+	}
+});
+
+
