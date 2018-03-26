f0c497c3fb97d88dd2476c454ee321f0ea6b33ae
Antony Damico <antonydamico29@gmail.com>
2018-03-26 15:09:15 -0400

    final stylings

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,9 +15,14 @@ $("ul").on("click", "span", function(e){
 $("input[type='text']").keypress(function(e){
 	if(e.which === 13){
 		var todo = $(this).val();
-		$("ul").append("<li><span>x</span> "+ todo + "</li>");
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> "+ todo + "</li>");
 		$(this).val("");
 	}
 });
+
+
+$(".fa-plus").click(function(){
+	$('input[type="text"]').fadeToggle();
+})
 
 
